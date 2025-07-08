894a30f4423abef6e3652ef7c14c39553e00070d
Karim Al-Zmzmy <karam.zmzmy@gmail.com>
2025-07-08 04:04:52 +0300

    add some needed fonts

--- app/src/main/java/com/example/caffeine/ui/theme/Type.kt
+++ app/src/main/java/com/example/caffeine/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.caffeine.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.caffeine.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,14 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val Sniglet = FontFamily(
+    Font(R.font.sniglet_regular, FontWeight.Normal),
+    Font(R.font.sniglet_extra_bold, FontWeight.Bold)
+)
+val Urbanist = FontFamily(
+    Font(R.font.urbanist_bold, FontWeight.Bold),
+    Font(R.font.urbanist_medium, FontWeight.Medium)
+
 )
